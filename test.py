144aee8232263ff6509568347e930d384c3cd775
kathakansal <89076234+kathakansal@users.noreply.github.com>
2022-09-12 15:46:37 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+hile True:
+    a = int(input('ENTER A NUMBER: '))
+    try:
+        print(100/a)
+        break
+    except:
+        print('not divisible by 100')
+    finally:
+        print('end of program')
